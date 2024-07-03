9a4d3f8a0220366780a7df220aed84b625ab72be
zm7679 <zaidmomin518@gmail.com>
2024-07-04 00:34:05 +0530

    Create String_Manipulation.py

---
+++ Task 1/String_Manipulation.py
@@ -0,0 +1,10 @@
+#String Manipulation
+# Concatenation, slicing, length, upper/lower case
+my_string = "Hello, World!"
+concatenated = my_string + " How are you?"
+sliced = my_string[7:12]
+length = len(my_string)
+upper_case = my_string.upper()
+lower_case = my_string.lower()
+
+print(f"Concatenated: {concatenated}, Sliced: {sliced}, Length: {length}, Upper case: {upper_case}, Lower case: {lower_case}")
