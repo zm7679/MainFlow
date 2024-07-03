fbaa097c21adf1295e0eaaa8d28d2862d09954ad
zm7679 <zaidmomin518@gmail.com>
2024-07-04 00:34:32 +0530

    Create Conditional_Statements.py

---
+++ Task 1/Conditional_Statements.py
@@ -0,0 +1,11 @@
+#Conditional Statements
+# If, elif, else
+x = 10
+y = 20
+
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is less than y")
+else:
+    print("x is equal to y")
