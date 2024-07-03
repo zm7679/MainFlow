964cea31db6134356add5499f2f0c8cb6c72b478
zm7679 <zaidmomin518@gmail.com>
2024-07-04 00:33:25 +0530

    Create Arithmetic_Operations.py

---
+++ Task 1/Arithmetic_Operations.py
@@ -0,0 +1,10 @@
+#Arithmetic Operations
+# Addition, subtraction, multiplication, division
+a = 10
+b = 5
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+
+print(f"Sum: {sum}, Difference: {difference}, Product: {product}, Quotient: {quotient}")
